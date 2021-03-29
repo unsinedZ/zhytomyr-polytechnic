6242d9ebb4091c4e840916cdc3147b692e9d2d2a
Vlad Franchuk <34125282+extreamer-lynx@users.noreply.github.com>
2021-03-29 13:53:14 +0300

    Add localization (#2)

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -1,68 +1,20 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:zhytomyr_polytechnic/app.dart';
+import 'package:zhytomyr_polytechnic/app_constants.dart';
 
-import 'app_context.dart';
-
-void main() {
-  runApp(MyApp());
+Future<void> main() async {
+  await initLocalizationAsync();
+  runApp(
+    EasyLocalization(
+        supportedLocales: [Locale('uk'), Locale('en')],
+        path: AppConstants.translationPath,
+        fallbackLocale: Locale('en'),
+        child: App()),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: AppContext.appName,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: AppContext.appName),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
-  }
+Future<void> initLocalizationAsync() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
 }
