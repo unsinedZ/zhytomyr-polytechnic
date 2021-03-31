79307104ea3d84e87c1c93d0ddb7ad48374a898d
Vlad Franchuk <franchuk@gmail.com>
2021-03-31 15:38:27 +0300

    Fixed test on faculty_list

--- src/features/faculty_list/test/bl/models/faculty_model_test.dart
+++ src/features/faculty_list/test/bl/models/faculty_model_test.dart
@@ -4,8 +4,8 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   test("Faculty.fromJson work correctly", () {
     final Faculty faculty =
-        Faculty.fromJson({"id": "1", "name": "name", "imageUrl": "imageUrl"});
-    expect(faculty.id, "1");
+        Faculty.fromJson({"id": 1, "name": "name", "imageUrl": "imageUrl"});
+    expect(faculty.id, 1);
     expect(faculty.name, "name");
     expect(faculty.imageUrl, "imageUrl");
   });
