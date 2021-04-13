04661ab0f2820b0f4c26d658286192e07c71df4d
Андрій Колесник <andriykolesnuk2000@gmail.com>
2021-04-13 14:35:06 +0300

    Add activity info dialog

--- src/features/timetable/lib/src/widgets/components/activity_info_dialog.dart
+++ src/features/timetable/lib/src/widgets/components/activity_info_dialog.dart
@@ -8,14 +8,86 @@ class ActivityInfoDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(activity.name),
-        Text(activity.tutor.name),
-        Text(activity.groups.map((group) => group.name).join(', ')),
-        //.fold('', (previousValue, group) => previousValue + ' ,' + group.name)),
-        Text(activity.room),
-      ],
+    return AlertDialog(
+      insetPadding: EdgeInsets.zero,
+      titlePadding: EdgeInsets.zero,
+      contentPadding: EdgeInsets.zero,
+      content: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: Text(
+                    activity.name,
+                    textScaleFactor: 1.5,
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                RichText(
+                  textScaleFactor: 1.1,
+                  text: TextSpan(
+                    text: 'Teacher: ',
+                    style: Theme.of(context).textTheme.subtitle1,
+                    children: <TextSpan>[
+                      TextSpan(
+                          text: activity.tutor.name,
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                RichText(
+                  textScaleFactor: 1.1,
+                  text: TextSpan(
+                    text: 'Groups: ',
+                    style: Theme.of(context).textTheme.subtitle1,
+                    children: <TextSpan>[
+                      TextSpan(
+                          text: activity.groups
+                              .map((group) => group.name)
+                              .join(', '),
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                RichText(
+                  textScaleFactor: 1.1,
+                  text: TextSpan(
+                    text: 'Auditory: ',
+                    style: Theme.of(context).textTheme.subtitle1,
+                    children: <TextSpan>[
+                      TextSpan(
+                          text: activity.room,
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            right: 0,
+            top: 0,
+            child: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: Icon(Icons.close),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
