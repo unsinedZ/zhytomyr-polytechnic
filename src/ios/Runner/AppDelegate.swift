455d16e16faa18920c2678bff9ca340444a13ed4
krida2000 <andriykolesnuk2000@gmail.com>
2021-05-06 17:26:42 +0300

    Add ios pods

--- src/ios/Runner/AppDelegate.swift
+++ src/ios/Runner/AppDelegate.swift
@@ -1,9 +1,11 @@
 import UIKit
 import Flutter
 import Firebase
+import GoogleSignIn
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate{
+  
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
