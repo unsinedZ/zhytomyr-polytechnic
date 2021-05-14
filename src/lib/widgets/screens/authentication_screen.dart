8638114c20637e00bbf6ef0bd2f31777a66ed742
Андрій Колесник <andriykolesnuk2000@gmail.com>
2021-05-14 12:54:08 +0300

    Fix authentication screen bug

--- src/lib/widgets/screens/authentication_screen.dart
+++ src/lib/widgets/screens/authentication_screen.dart
@@ -47,70 +47,70 @@ class _AuthenticationScreenState extends State<AuthenticationScreen> {
   }
 
   @override
-  Widget build(BuildContext context) => SafeArea(
-        child: Scaffold(
-          body: StreamBuilder<User?>(
-              stream: context.read<UserSyncBloc>().mappedUser,
-              builder: (context, snapshot) {
-                if (!snapshot.hasData || !snapshot.data!.isEmpty) {
-                  return Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
+  Widget build(BuildContext context) => Scaffold(
+    body: SafeArea(
+      child: StreamBuilder<User?>(
+          stream: context.read<UserSyncBloc>().mappedUser,
+          builder: (context, snapshot) {
+            if (!snapshot.hasData || !snapshot.data!.isEmpty) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
 
-                return Padding(
-                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                  child: Center(
-                    child: SingleChildScrollView(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
+            return Padding(
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: Center(
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        'assets/images/logo.png',
+                        width: MediaQuery.of(context).size.width * 0.7,
+                      ),
+                      SizedBox(
+                        height: 90,
+                      ),
+                      Wrap(
+                        alignment: WrapAlignment.center,
                         children: [
-                          Image.asset(
-                            'assets/images/logo.png',
-                            width: MediaQuery.of(context).size.width * 0.7,
-                          ),
-                          SizedBox(
-                            height: 90,
-                          ),
-                          Wrap(
-                            alignment: WrapAlignment.center,
-                            children: [
-                              Text(textLocalizer
-                                  .localize('By authorize you agree to ')),
-                              InkWell(
-                                child: Text(
-                                  textLocalizer.localize('terms & conditions.'),
-                                  style: Theme.of(context).textTheme.headline4,
-                                ),
-                                onTap: () => Navigator.pushNamed(
-                                    context, '/terms&conditions'),
-                              ),
-                            ],
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          GoogleSignInButton(
-                            authenticationBloc:
-                                context.read<GoogleAuthenticationBloc>(),
-                          ),
-                          if (Platform.isIOS) ...[
-                            SizedBox(
-                              height: 10,
+                          Text(textLocalizer
+                              .localize('By authorize you agree to ')),
+                          InkWell(
+                            child: Text(
+                              textLocalizer.localize('terms & conditions.'),
+                              style: Theme.of(context).textTheme.headline4,
                             ),
-                            AppleSignInButton(
-                              appleAuthenticationBloc:
-                                  context.read<AppleAuthenticationBloc>(),
-                            ),
-                          ],
+                            onTap: () => Navigator.pushNamed(
+                                context, '/terms&conditions'),
+                          ),
                         ],
                       ),
-                    ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      GoogleSignInButton(
+                        authenticationBloc:
+                            context.read<GoogleAuthenticationBloc>(),
+                      ),
+                      if (Platform.isIOS) ...[
+                        SizedBox(
+                          height: 10,
+                        ),
+                        AppleSignInButton(
+                          appleAuthenticationBloc:
+                              context.read<AppleAuthenticationBloc>(),
+                        ),
+                      ],
+                    ],
                   ),
-                );
-              }),
-        ),
-      );
+                ),
+              ),
+            );
+          }),
+    ),
+  );
 
   @override
   void dispose() {
