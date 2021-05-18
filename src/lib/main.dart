0a89e8509a9ff8325ca81d1a0f0f8d0e8bdd859b
Андрій Колесник <andriykolesnuk2000@gmail.com>
2021-05-18 11:55:18 +0300

    Add crashlytics to android

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -1,30 +1,44 @@
+import 'dart:async';
+import 'dart:isolate';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
 import 'package:push_notification/push_notification.dart';
 import 'package:zhytomyr_polytechnic/widgets/app.dart';
 
 Future<void> main() async {
   await initLocalizationAsync();
-  runApp(
-    Phoenix(
-      child: EasyLocalization(
-          supportedLocales: [Locale('uk'), Locale('en')],
-          path: "assets/translations",
-          fallbackLocale: Locale('uk'),
-          child: App()),
-    ),
-  );
+  runZonedGuarded(() {
+    runApp(
+      Phoenix(
+        child: EasyLocalization(
+            supportedLocales: [Locale('uk'), Locale('en')],
+            path: "assets/translations",
+            fallbackLocale: Locale('uk'),
+            child: App()),
+      ),
+    );
+  }, FirebaseCrashlytics.instance.recordError);
 }
 
 Future<void> initLocalizationAsync() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
   await Firebase.initializeApp();
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+  Isolate.current.addErrorListener(RawReceivePort((pair) async {
+    final List<dynamic> errorAndStacktrace = pair;
+    await FirebaseCrashlytics.instance.recordError(
+      errorAndStacktrace.first,
+      errorAndStacktrace.last,
+    );
+  }).sendPort);
   initNotification();
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 }
