8e6229ff11a655c3ceefddf08cbf10b7aa028515
Андрій Колесник <andriykolesnuk2000@gmail.com>
2021-07-02 12:30:53 +0300

    Imports format

--- src/lib/bl/repositories/group_timetable_firestore_repository.dart
+++ src/lib/bl/repositories/group_timetable_firestore_repository.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
-
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:timetable/timetable.dart';
